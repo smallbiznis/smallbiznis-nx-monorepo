33a8a5ac947a470164a31d380ff1dcf6a040952e
Taufik Triantono <taufiktriantono4@gmail.com>
2025-12-11 02:47:25 +0700

    Revert console next.config to default

--- apps/console/next.config.js
+++ apps/console/next.config.js
@@ -1,35 +1,13 @@
 //@ts-check
 
-const webpack = require('webpack');
 const { composePlugins, withNx } = require('@nx/next');
-
-const isCloudEdition = process.env.SMALLBIZNIS_EDITION === "cloud";
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  // Use this to set Nx-specific options
   nx: {},
-  async redirects() {
-    if (isCloudEdition) {
-      return [];
-    }
-
-    return [
-      {
-        source: '/internal-admin/:path*',
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
-  webpack: (config) => {
-    if (!isCloudEdition) {
-      config.plugins = config.plugins || [];
-      config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /internal-admin/ }));
-    }
-    return config;
-  },
 };
 
 const plugins = [
